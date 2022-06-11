a7888a566ee4269f41ae8054729a43d5ad0cf2ac
Creakzyy <103847718+Creakzyy@users.noreply.github.com>
2022-06-11 12:37:21 -0400

    Add files via upload

---
+++ ForLoops_and_ Rounding.py
@@ -0,0 +1,13 @@
+# x = round (5.782937456,2)
+#print(x)
+
+#for x in "Toronto":
+    #print(x)
+
+x = ["pen", "pencil", "bottle"]
+
+for y in x:
+    print(y)
+    if y == "pencil":
+
+        break
