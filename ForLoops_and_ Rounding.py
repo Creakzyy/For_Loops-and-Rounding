8f5a16cbe00b77399aaee50104b4e3f8a22dc984
Creakzyy <103847718+Creakzyy@users.noreply.github.com>
2022-06-11 12:45:08 -0400

    Add files via upload

--- ForLoops_and_ Rounding.py
+++ ForLoops_and_ Rounding.py
@@ -1,13 +1,27 @@
 # x = round (5.782937456,2)
 #print(x)
 
+
+
 #for x in "Toronto":
     #print(x)
 
+
+
+"""
 x = ["pen", "pencil", "bottle"]
 
 for y in x:
     print(y)
     if y == "pencil":
 
-        break
+        break """
+
+
+
+prices = [3.44, 9.99, 2.50, 20.00]
+total = 0
+for price in prices:
+    print("Item costs $", price)
+    total = total+price
+print("shopping total", round(total, 2))
